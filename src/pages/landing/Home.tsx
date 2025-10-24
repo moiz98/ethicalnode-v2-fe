36caf73ec23c0273d32d6157c4c53e420bdea7d4
moiz <memon1998@yahoo.com>
2025-10-25 01:14:08 +0500

    fix: update condition to check for non-empty old version URL in Home component

--- src/pages/landing/Home.tsx
+++ src/pages/landing/Home.tsx
@@ -61,7 +61,7 @@ const Home = () => {
     )} */}
     
     {/* Welcome Modal - only show if old version URL is configured */}
-    {import.meta.env.VITE_OLD_VERSION_URL && (
+    {import.meta.env.VITE_OLD_VERSION_URL != '' && (
       <>
         <WelcomeModal 
           isOpen={showWelcomeModal} 
